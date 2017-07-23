97ce5aaa2a9a3fe6740861701ef4fa5dc5142744
Zachary Becker <zrbecker@gmail.com>
2017-07-22 18:24:32 -0700

    consolidated configuration

--- sqlalchemy_playground/app.py
+++ sqlalchemy_playground/app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+class DevelopmentConfig:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config.from_object(DevelopmentConfig)
 db = SQLAlchemy(app)
 
 from . import views
