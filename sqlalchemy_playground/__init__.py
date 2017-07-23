b02c1c81a998621a0988a16740ae76ceca8c2f10
Zachary Becker <zrbecker@gmail.com>
2017-07-22 18:43:32 -0700

    tables with relationships

--- sqlalchemy_playground/__init__.py
+++ sqlalchemy_playground/__init__.py
@@ -1,7 +1,9 @@
 from .app import app, db
+from .models import User, Post, Category
 
 @app.cli.command()
 def init_schema():
+    db.drop_all()
     db.create_all()
 
 @app.cli.command()
@@ -11,4 +13,17 @@ def init_test_data():
 
     db.session.add(admin)
     db.session.add(guest)
+
+    cat1 = Category('python')
+    cat2 = Category('c++')
+    p1 = Post('Hello Python!', 'Python is pretty cool', cat1)
+    p2 = Post('Hello Python Again!', 'Python is pretty cool', cat1)
+    p3 = Post('Hello C++!', 'C++ is pretty cool', cat2)
+
+    db.session.add(cat1)
+    db.session.add(cat2)
+    db.session.add(p1)
+    db.session.add(p2)
+    db.session.add(p3)
+
     db.session.commit()
--- sqlalchemy_playground/views.py
+++ sqlalchemy_playground/views.py
@@ -1,10 +1,23 @@
 from flask import escape
 
 from .app import app, db
-from .models import User
+from .models import User, Post, Category
 
 @app.route('/')
 def index():
     users = User.query.all()
     admin = User.query.filter_by(username='admin').first()
     return escape(str(users)) + '<br>' + escape(str(admin))
+
+@app.route('/posts/')
+@app.route('/posts/<category_name>')
+def posts(category_name=None):
+    if category_name is None:
+        posts = Post.query.all()
+        return escape(str(posts))
+    else:
+        category = Category.query.filter_by(name=category_name).first()
+        if category is None:
+            return 'No Posts'
+        else:
+            return escape(str(category.posts.all()))
